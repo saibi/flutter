549241b4da7ccee01aad810cfa20937127aa917e
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-17 16:10:12 +0900

    nomadcoder: Hero animation

--- webtoon/lib/widgets/webtoon_widget.dart
+++ webtoon/lib/widgets/webtoon_widget.dart
@@ -28,35 +28,38 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                )
-              ],
-            ),
-            clipBehavior: Clip.hardEdge,
-            child: Image.network(
-              thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Container(
-                  color: Colors.amber,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Whoops!',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                );
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(
+                thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.amber,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Whoops!',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  );
+                },
+              ),
             ),
           ),
           const SizedBox(
