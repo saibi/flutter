a7d549593d07251182b01e451a8a60ab1c12d1c3
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-18 10:13:28 +0900

    nomadcoder: url launcher

---
+++ webtoon/lib/widgets/episode_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  final WebtoonEpisodeModel episode;
+  final String webtoon_id;
+
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoon_id,
+  });
+
+  onButtonTap() async {
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoon_id&no=${episode.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          bottom: 10,
+        ),
+        decoration: BoxDecoration(
+            color: Colors.green.shade400,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                offset: const Offset(5, 5),
+                color: Colors.black.withOpacity(0.5),
+              )
+            ],
+            border: Border.all(
+              color: Colors.green.shade500,
+              width: 1,
+            )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
