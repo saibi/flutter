dafb4ee545d8841a18e23efae92876a52bd8c06f
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-31 17:25:00 +0900

    max ch03: 55, challenge solution

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,63 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/start_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const QuizHome(),
-    );
-  }
-}
-
-class QuizHome extends StatelessWidget {
-  const QuizHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-            ),
-            const Text(
-              'Learn Flutter the fun way!',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text(
-                'Start Quiz',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurple,
+              Colors.deepPurple.shade900,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
+        child: const StartScreen(),
       ),
-    );
-  }
+    ),
+  ));
 }
