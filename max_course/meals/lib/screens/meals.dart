6121159a08a2bcbc185935fa652bab72e60d02f3
Youngmin Kim <kimyoungmin@gmail.com>
2023-06-20 17:58:43 +0900

    max 08: ~161, my detail screen

--- max_course/meals/lib/screens/meals.dart
+++ max_course/meals/lib/screens/meals.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/details.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void _selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => DetailsScreen(
+        meal: meal,
+      ),
+    )); // Navigator.push(context, route)
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +42,11 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-          meals[index].title,
-          style: const TextStyle(color: Colors.white),
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectItem: () {
+            _selectMeal(context, meals[index]);
+          },
         ),
       );
     }
