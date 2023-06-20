a67189839ee7214c22dee68b3c92851ab4cb2fbf
Youngmin Kim <kimyoungmin@gmail.com>
2023-06-20 16:44:44 +0900

    max 08: ~158 Navigator

--- max_course/meals/lib/screens/categories.dart
+++ max_course/meals/lib/screens/categories.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filterredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => MealsScreen(
+        title: category.title,
+        meals: filterredMeals,
+      ),
+    )); // Navigator.push(context, route)
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +36,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category:category)).toList()
           for (final category in availableCategorids)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
---
+++ max_course/meals/lib/widgets/meal_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({
+    super.key,
+    required this.meal,
+  });
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: const Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
