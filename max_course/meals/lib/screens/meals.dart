977072d42ed70e15ee31426f87d12bfc3dd30299
Youngmin Kim <kimyoungmin@gmail.com>
2023-06-21 09:19:38 +0900

    max 08: ~163, AppBar, BottomNavigationBar

--- max_course/meals/lib/screens/meals.dart
+++ max_course/meals/lib/screens/meals.dart
@@ -4,9 +4,13 @@ import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void _selectMeal(BuildContext context, Meal meal) {
@@ -50,9 +54,14 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
