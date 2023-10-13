c27b3efc247b6b9fa5d313fb39493d617eeeed16
Youngmin Kim <kimyoungmin@gmail.com>
2023-10-13 18:38:33 +0900

    dangn market clone coding #8: carousel_slider

--- clone_coding/carrot_market_sample/lib/page/detail.dart
+++ clone_coding/carrot_market_sample/lib/page/detail.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
@@ -8,28 +9,99 @@ class DetailContentView extends StatefulWidget {
   State<DetailContentView> createState() => _DetailContentViewState();
 }
 
+const imageMax = 5;
+
 class _DetailContentViewState extends State<DetailContentView> {
+  late Size size;
+  int _current = 0;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    size = MediaQuery.of(context).size;
+  }
+
   PreferredSizeWidget _appBarWidget() {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back, color: Colors.white)),
       actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.share, color: Colors.white)),
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert, color: Colors.white)),
       ],
     );
   }
 
   Widget _bodyWidget() {
     return Container(
-      child: Hero(
-        tag: widget.data["cid"]!,
-        child: Image.asset(
-          widget.data["image"]!,
-          fit: BoxFit.fill,
-          width: double.infinity,
-        ),
+      child: Stack(
+        children: [
+          Hero(
+            tag: widget.data["cid"]!,
+            child: CarouselSlider(
+              items: List.generate(imageMax, (index) {
+                return Image.asset(
+                  widget.data["image"]!,
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                );
+              }),
+              options: CarouselOptions(
+                autoPlay: true,
+                // enlargeCenterPage: true,
+                viewportFraction: 1.0,
+                initialPage: 0,
+                height: size.width,
+                onPageChanged: (index, reason) {
+                  print(index);
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(imageMax, (index) {
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == index
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.4),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _bottomBarWidget() {
+    return Container(
+      height: 55,
+      width: double.infinity,
+      color: Colors.red,
     );
   }
 
@@ -39,6 +111,7 @@ class _DetailContentViewState extends State<DetailContentView> {
       extendBodyBehindAppBar: true,
       appBar: _appBarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomBarWidget(),
     );
   }
 }
